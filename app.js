798277d89b2d7a5120c4a36e3d7faf3b7789b497
funkyblues <dnr8874@naver.com>
2022-03-17 01:18:46 +0900

    페이지 로그인 확인을 위한 Global View Variable

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const logger = require('morgan')
-const bodyParser = require('body-parser')
+const express = require("express");
+const nunjucks = require("nunjucks");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 
+const admin = require("./routes/admin");
+const contacts = require("./routes/contacts");
 
-const admin = require('./routes/admin')
-const contacts = require('./routes/contacts')
+const app = express();
+const PORT = 3000;
 
-
-const app = express()
-const PORT = 3000
-
-nunjucks.configure('template', {
-  autoescape : true,
-  express : app
-})
+nunjucks.configure("template", {
+  autoescape: true,
+  express: app,
+});
 
 // middleware
-app.use( logger('dev') )
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded({ extended:false }) )
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use( '/uploads', express.static('uploads') );
+app.use("/uploads", express.static("uploads"));
 
+app.use((req, res, next) => {
+  app.locals.isLogin = false;
+  next();
+});
 
-app.get('/', (req, res) => {
-  res.send('hello express')
-})
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
 
 function vipMiddleware(req, res, next) {
-  console.log('최우선 미들웨어')
-  next()
+  console.log("최우선 미들웨어");
+  next();
 }
 
-app.use( '/admin', vipMiddleware, admin )
-app.use( '/contacts', contacts)
+app.use("/admin", vipMiddleware, admin);
+app.use("/contacts", contacts);
 
-app.listen( PORT , () => {
-  console.log('Express listening on port ', PORT)
-})
+app.listen(PORT, () => {
+  console.log("Express listening on port ", PORT);
+});
--- routes/admin.js
+++ routes/admin.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 function testMiddleware(req, res, next) {
-  console.log('첫 번째 미들웨어');
-  next()
+  console.log("첫 번째 미들웨어");
+  next();
 }
 
 function testMiddleware2(req, res, next) {
-  console.log('두 번째 미들웨어');
-  next()
+  console.log("두 번째 미들웨어");
+  next();
 }
 
+router.get("/", testMiddleware, testMiddleware2, (req, res) => {
+  res.send("admin 이후 url");
+});
 
-router.get('/', testMiddleware, testMiddleware2, (req, res) => {
-  res.send('admin 이후 url')
-})
-
-router.get('/products', (req, res) => {
-  res.render( 'admin/products.html' , {
+router.get("/products", (req, res) => {
+  res.render("admin/products.html", {
     message: `hello`,
-  })
-})
+  });
+});
 
-router.get('/products/write', (req, res) => {
-  res.render('admin/write.html')
-})
+router.get("/products/write", (req, res) => {
+  res.render("admin/write.html");
+});
 
-router.post('/products/write', (req, res) => {
-  res.send(req.body)
-})
+router.post("/products/write", (req, res) => {
+  res.send(req.body);
+});
 
-
-module.exports = router
+module.exports = router;
